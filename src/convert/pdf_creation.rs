0cfad9508bb5b3599bf84e83da6a9d8e7b684b74
Johan <jvandewallemuniz@gmail.com>
2023-10-07 16:15:27 +0000

    adds the first images only to debug

--- src/convert/pdf_creation.rs
+++ src/convert/pdf_creation.rs
@@ -5,7 +5,10 @@ use std::{
 };
 
 use anyhow::{bail, Result};
-use printpdf::{Mm, PdfDocument, PdfDocumentReference};
+use printpdf::{image_crate, Image, ImageTransform, Mm, PdfDocument, PdfDocumentReference};
+
+const SIZE_X: Mm = Mm(247.0);
+const SIZE_Y: Mm = Mm(210.0);
 
 pub fn create_pdf_file(file_path: &Path, images: Vec<PathBuf>) -> Result<()> {
     if file_path.file_stem().is_none() {
@@ -18,7 +21,7 @@ pub fn create_pdf_file(file_path: &Path, images: Vec<PathBuf>) -> Result<()> {
         bail!("unable to get the original file name")
     }
 
-    let (doc, _, _) = PdfDocument::new(file_name.unwrap(), Mm(247.0), Mm(210.0), "Layer 1");
+    let (doc, _, _) = PdfDocument::new(file_name.unwrap(), SIZE_X, SIZE_Y, "Layer 1");
 
     populate_file(&doc, images)?;
 
@@ -27,6 +30,47 @@ pub fn create_pdf_file(file_path: &Path, images: Vec<PathBuf>) -> Result<()> {
     Ok(())
 }
 
-fn populate_file(_doc: &PdfDocumentReference, _images: Vec<PathBuf>) -> Result<()> {
+fn populate_file(doc: &PdfDocumentReference, images: Vec<PathBuf>) -> Result<()> {
+    let mut index = 0;
+
+    for image_path in images {
+        if image_path.extension().is_none() || image_path.extension().unwrap().to_str().is_none() {
+            println!("skipping image: {:?} -> Unable to get format", image_path);
+            continue;
+        }
+
+        let image = get_image(&image_path)?;
+        if image.is_none() {
+            println!("skipping image: {:?} -> Format not supported", image_path);
+        }
+        let image = image.unwrap();
+
+        let (page_index, layer_index) = doc.add_page(SIZE_X, SIZE_Y, "");
+        let current_layer = doc.get_page(page_index).get_layer(layer_index);
+        image.add_to_layer(current_layer.clone(), ImageTransform::default());
+
+        index += 1;
+        if index > 10 {
+            break;
+        }
+    }
+
     Ok(())
+}
+
+fn get_image(image_path: &PathBuf) -> Result<Option<Image>> {
+    let image_extension = image_path.extension().unwrap().to_str().unwrap();
+
+    let image_file = File::open(image_path).unwrap();
+    let image = match image_extension {
+        "png" => Some(Image::try_from(image_crate::codecs::png::PngDecoder::new(
+            image_file,
+        )?)?),
+        "jpg" => Some(Image::try_from(
+            image_crate::codecs::jpeg::JpegDecoder::new(image_file)?,
+        )?),
+        _ => None,
+    };
+
+    Ok(image)
 }
