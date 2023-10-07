df035eb46071e768250b82638ecc00ba98be7eca
Johan <jvandewallemuniz@gmail.com>
2023-10-07 19:32:58 +0000

    scales images accordingly

--- src/convert/pdf_creation.rs
+++ src/convert/pdf_creation.rs
@@ -7,8 +7,14 @@ use std::{
 use anyhow::{bail, Result};
 use printpdf::{image_crate, Image, ImageTransform, Mm, PdfDocument, PdfDocumentReference};
 
-const SIZE_X: Mm = Mm(247.0);
-const SIZE_Y: Mm = Mm(210.0);
+const MANGA_WIDTH: f32 = 127.0;
+const DOUBLE_MANGA_WIDTH: f32 = 127.0 * 2.0;
+const MANGA_HEIGHT: f32 = 190.5;
+
+const DPI: f32 = 300.0;
+
+const MM_PER_INCH: f32 = 25.4;
+const MM_PER_DPI: f32 = MM_PER_INCH / DPI;
 
 pub fn create_pdf_file(file_path: &Path, images: Vec<PathBuf>) -> Result<()> {
     if file_path.file_stem().is_none() {
@@ -21,8 +27,12 @@ pub fn create_pdf_file(file_path: &Path, images: Vec<PathBuf>) -> Result<()> {
         bail!("unable to get the original file name")
     }
 
-    let (doc, _, _) = PdfDocument::new(file_name.unwrap(), SIZE_X, SIZE_Y, "Layer 1");
-
+    let (doc, _, _) = PdfDocument::new(
+        file_name.unwrap(),
+        Mm(DOUBLE_MANGA_WIDTH),
+        Mm(MANGA_HEIGHT),
+        "",
+    );
     populate_file(&doc, images)?;
 
     doc.save(&mut BufWriter::new(File::create(file_path).unwrap()))?;
@@ -34,20 +44,29 @@ fn populate_file(doc: &PdfDocumentReference, images: Vec<PathBuf>) -> Result<()>
     let mut index = 0;
 
     for image_path in images {
-        if image_path.extension().is_none() || image_path.extension().unwrap().to_str().is_none() {
-            println!("skipping image: {:?} -> Unable to get format", image_path);
-            continue;
-        }
-
+        println!("Processing {:?}", image_path);
         let image = get_image(&image_path)?;
         if image.is_none() {
             println!("skipping image: {:?} -> Format not supported", image_path);
+            continue;
         }
         let image = image.unwrap();
 
-        let (page_index, layer_index) = doc.add_page(SIZE_X, SIZE_Y, "");
+        let scale_factor = get_correct_scale_factor(&image)?;
+
+        let (page_index, layer_index) = doc.add_page(Mm(DOUBLE_MANGA_WIDTH), Mm(MANGA_HEIGHT), "");
         let current_layer = doc.get_page(page_index).get_layer(layer_index);
-        image.add_to_layer(current_layer.clone(), ImageTransform::default());
+
+        let transform = ImageTransform {
+            translate_x: None,
+            translate_y: None,
+            rotate: None,
+            scale_x: Some(scale_factor),
+            scale_y: Some(scale_factor),
+            dpi: Some(DPI),
+        };
+
+        image.add_to_layer(current_layer.clone(), transform);
 
         index += 1;
         if index > 10 {
@@ -59,6 +78,10 @@ fn populate_file(doc: &PdfDocumentReference, images: Vec<PathBuf>) -> Result<()>
 }
 
 fn get_image(image_path: &PathBuf) -> Result<Option<Image>> {
+    if image_path.extension().is_none() || image_path.extension().unwrap().to_str().is_none() {
+        return Ok(None);
+    }
+
     let image_extension = image_path.extension().unwrap().to_str().unwrap();
 
     let image_file = File::open(image_path).unwrap();
@@ -73,4 +96,44 @@ fn get_image(image_path: &PathBuf) -> Result<Option<Image>> {
     };
 
     Ok(image)
+}
+
+fn get_correct_scale_factor(image: &Image) -> Result<f32> {
+    let mut is_double = false;
+    let width_in_mm = image.image.width.0 as f32 * MM_PER_DPI;
+    let height_in_mm = image.image.height.0 as f32 * MM_PER_DPI;
+
+    let width_scale_factor = match height_in_mm > width_in_mm {
+        true => MANGA_WIDTH / width_in_mm,
+        false => {
+            is_double = true;
+            DOUBLE_MANGA_WIDTH / width_in_mm
+        }
+    };
+
+    let mut scale_factor = width_scale_factor;
+    if !is_in_manga_bounds_after_scale(width_in_mm, height_in_mm, width_scale_factor, is_double) {
+        scale_factor = MANGA_HEIGHT / height_in_mm;
+    }
+
+    return Ok(scale_factor);
+}
+
+fn is_in_manga_bounds_after_scale(
+    width: f32,
+    height: f32,
+    scale_factor: f32,
+    is_double: bool,
+) -> bool {
+    let leeway = 0.5;
+
+    let manga_width = match is_double {
+        true => DOUBLE_MANGA_WIDTH,
+        false => MANGA_WIDTH,
+    };
+
+    let scaled_width = width * scale_factor;
+    let scaled_height = height * scale_factor;
+
+    (scaled_width - leeway) < manga_width && (scaled_height - leeway) < MANGA_HEIGHT
 }
